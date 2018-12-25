c7094bf0dac6806dd4da47c65859a997835ee446
Lucas Petti <luc_petti@hotmail.com>
2018-12-25 17:26:41 +0100

    updates

--- to_add.rb
+++ to_add.rb
@@ -2,8 +2,8 @@
 puts 'English - '
 puts 'French - '
 puts 'German - '
-puts 'Italian - Ludovico Ariosto, Michelangelo, Moderata Fonte'
-puts 'Portuguese - Caetano Veloso, João Cabral de Melo Neto, Gonçalves Dias, Alda do Espirito Santo, Angélica Freitas, Ricardo Domeneck'
+puts 'Italian - Michelangelo, Moderata Fonte'
+puts 'Portuguese - Caetano Veloso, Gonçalves Dias, Alda do Espirito Santo, Angélica Freitas, Ricardo Domeneck'
 puts 'Spanish - Antonio Machado'
 puts ' '
 # Sources to use:
