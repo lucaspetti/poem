96e3602914c7aa003833ebf5793a428bee71537f
pettil <lucas.petti@devolute.org>
2019-09-13 17:05:40 +0200

    Include jsx components

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const header = (
+  <header>
+    <h1 id="main-title">POEM</h1>
+  </header>
+)
+
+ReactDOM.render(header, rootElement)
---
+++ src/index.js
@@ -0,0 +1,1 @@
+render(<App />, document.querySelector('#main'));
