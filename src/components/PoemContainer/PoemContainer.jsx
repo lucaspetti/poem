7b4b1059886246ae153f0931605cd5deae0702bd
lucaspetti <luc_petti@hotmail.com>
2019-09-18 19:26:42 +0200

    Include image on poem container

--- src/components/PoemContainer/PoemContainer.jsx
+++ src/components/PoemContainer/PoemContainer.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const PoemContainer = (props) => (
-  <div className="container" id={props.id}>
+  <div className="container" id="poem-container">
 		<p id="poem"></p>
-		<div id="img-container"></div>
+		<div id="img-container">
+			<img
+				alt={`${props.language}-cover`}
+				width="672"
+				height="372"
+				src={`/images/covers/img-${props.language}.jpg`}
+			/>
+		</div>
 		<p className="poem-author"></p>
 	</div>
 )
